08bd1002cd0aad50c824a8638dfdc0606a056cff
cocktailpeanut <cocktailpeanuts@proton.me>
2025-12-04 17:51:02 -0500

    vibevoice-realtime

--- featured.js
+++ featured.js
@@ -6,6 +6,7 @@ module.exports = [
 //  },
 //    ["cocktailpeanut", "https://github.com/cocktailpeanutlabs/whisperspeech"],
 //    ["cocktailpeanut", "https://github.com/pinokiofactory/sd35"],
+  ["cocktailpeanut", "https://github.com/pinokiofactory/vibevoice-realtime"],
   ["SUP3RMASS1VE", "https://github.com/pinokiofactory/Ultimate-TTS-Studio"],
   ["cocktailpeanut", "https://github.com/pinokiofactory/openaudio"],
   ["henryruhs", "https://github.com/facefusion/facefusion-pinokio"],
